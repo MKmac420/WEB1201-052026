4ba9b03d1f0eae609c68a3215ea75f809333e3f1
MKmac420 <65330895+MKmac420@users.noreply.github.com>
2026-08-07 18:26:40 +0800

    Update global-nav.js

--- js/global-nav.js
+++ js/global-nav.js
@@ -20,3 +20,14 @@ function updateNavAuthButton() {
         navLoginBtn.textContent = 'LOGIN/REGISTER';
     }
 }
+
+/* Author: vimesh
+Desc: on mobile, shows or hides the hamburger menu when clicked 
+*/
+
+const menuButton = document.getElementById('mobile-menu-button');
+const navLinks = document.getElementById('nav-links');
+
+menuButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
